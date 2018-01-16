9c249957b363aa7662cac869f7d1a940a74162d1
Matt Mortensen <mortensenm1205@gmail.com>
2018-01-15 20:35:37 -0700

    after much trial and error with refactoring for reducing the amount callback f's I was only able to do so with call backs. I couldn't figure out how to make a function that downloaded a file sync. during the readdir async fs method

--- peopleMerger/people.js
+++ peopleMerger/people.js
@@ -1,40 +1,5 @@
 var fs = require('fs');
 
-var peoplePath = `${__dirname}/peopleFiles/`;
-
-
-//I'm trying to seperate my callbacks as regular functions.
-//I was able to write this one for downloading the files synchrously
-//this function just returns a string.
-function downloadFile(path) {
-  return fs.readFileSync(path, 'utf8');
-};
-
-function createFile() {};
-
-fs.readdir(peoplePath, 'utf8', (err, files) => {
-    if(err) {
-      throw err;
-    }
-    var fileData = "";
-    files.forEach((i) => {
-      //I can place the value of this f() in a variable since it's being
-      //synchrously
-      fileData = downloadFile(`${peoplePath}/${i}`);
-      //Converting to JSON
-      fileData = JSON.parse(fileData);
-      //concating upon itself and then sorting
-      fileData = fileData.concat(fileData).sort();
-    });
-
-    //Just a simple log to test, the next f() is writing that creates the file
-    //and uses the data from the first function as the content.
-    console.log(fileData);
-
-});
-
-
-
 //In my early code, it was outputing all the file contents as two different
 //values then I was having a hard time selecting it after.
 //I then guessed to download the contents of the files first SYNCHROUSLY and
@@ -42,32 +7,32 @@ fs.readdir(peoplePath, 'utf8', (err, files) => {
 //into a new one with .concat, since they returned back array's. After I had
 //that then I sorted it, and had the fs module write and read the file of the
 //two combined data files.
-// var file1 = fs.readFileSync('./peopleFiles/people1.json', 'utf8');
-// //parsing the data with JSON
-// file1 = JSON.parse(file1);
-//
-// var file2 = fs.readFileSync('./peopleFiles/people2.json', 'utf8');
-// //again here with a second file
-// file2 = JSON.parse(file2);
-//
-// //combining them into one array with .concat, then used .sort to sort through
-// //the data.
-// var allFile = file1.concat(file2);
-// allFile = allFile.sort();
-//
-// // console.log(typeof file1);
-// // console.log(typeof file2, file2);
-// // console.log(typeof allFile, allFile);
-//
-// fs.writeFile('./peopleFiles/allPeople.json', allFile, (err) => {
-//   if(err) {
-//     throw err;
-//   }
-//
-//   fs.readFile('./peopleFiles/allPeople.json', 'utf8', (err, data) => {
-//     console.log(data);
-//   });
-// });
+var file1 = fs.readFileSync('./peopleFiles/people1.json', 'utf8');
+//parsing the data with JSON
+file1 = JSON.parse(file1);
+
+var file2 = fs.readFileSync('./peopleFiles/people2.json', 'utf8');
+//again here with a second file
+file2 = JSON.parse(file2);
+
+//combining them into one array with .concat, then used .sort to sort through
+//the data.
+var allFile = file1.concat(file2);
+allFile = allFile.sort();
+
+// console.log(typeof file1);
+// console.log(typeof file2, file2);
+// console.log(typeof allFile, allFile);
+
+fs.writeFile('./peopleFiles/peopleComplete.json', allFile, (err) => {
+  if(err) {
+    throw err;
+  }
+
+  fs.readFile('./peopleFiles/peopleComplete.json', 'utf8', (err, data) => {
+    console.log(data);
+  });
+});
 
 
 
