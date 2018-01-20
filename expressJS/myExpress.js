b205798e6667a31653ac7e28df226a2e83802fe4
Matt Mortensen <mortensenm1205@gmail.com>
2018-01-20 15:12:24 -0700

    dependency now includesbody-parser, trying to understand post in express

--- expressJS/myExpress.js
+++ expressJS/myExpress.js
@@ -1,15 +1,46 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.write("Hello World");
-  res.end("<h1>Too Damn easy</h1>");
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+var post_data = {
+  "value" : "someString"
+}
+
+
+// function PostData(url) {
+//   var post_data = {
+//     "value" : "someString"
+//   }
+//
+//   return new Promise((resolve, reject) => {
+//     if(post_data === {}) {
+//       reject(new Error("whoops"));
+//     }
+//
+//       resolve(post_data);
+//
+//   });
+//
+//
+//
+// }
+//
+// app.get('/', (req, res) => {
+//   PostData('/').then((message) => {
+//     res.send(message);
+//   });
+// })
+
+app.post('/', (req, res) => {
+  req.body = post_data;
+  res.send(req.body);
 });
 
-app.post('/test', (req, res) => {
-  res.send("This data is being display through the server!");
-});
+app.get('/');
 
-app.listen(9000, () => {
-  console.log("This is now running on port 9000!");
+app.listen(7000, () => {
+  console.log("This is now running on port 7000!");
 });
